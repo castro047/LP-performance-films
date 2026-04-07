e24b7e65d05e711805bf2a6331bc6e366c2386b9
gustavo <105922570+castro047@users.noreply.github.com>
2026-04-06 22:26:15 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const menuItens = document.getElementById("menuItens");
+
+function menucelular() {
+    menuItens.classList.toggle("ativo");
+}
+
+// CARROSSEL
+let index = 0;
+
+const track = document.querySelector('.carousel-track');
+const slides = document.querySelectorAll('.carousel-track img');
+const totalSlides = slides.length;
+
+function mudarSlide() {
+    index++;
+
+    if (index >= totalSlides) {
+        index = 0;
+    }
+
+    track.style.transform = `translateX(-${index * 100}%)`;
+}
+
+const tempoParado = 5000; // 5s parado
+const tempoAnimacao = 1000; // 1s animação
+
+setInterval(mudarSlide, tempoParado);
+
